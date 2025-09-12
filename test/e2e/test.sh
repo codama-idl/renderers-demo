a91411f3325fc5ba1fef33b7cb126cacc0518f93
Loris Leiva <loris.leiva@gmail.com>
2025-09-12 10:49:41 +0100

    Rewire configurations

--- test/e2e/test.sh
+++ test/e2e/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eux
 
-(cd test/e2e/system && pnpm test)
-(cd test/e2e/token && pnpm test)
+(cd test/e2e/system && pnpm install && pnpm test)
+(cd test/e2e/token && pnpm install && pnpm test)
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,45 @@
-import { defineConfig } from 'tsup';
+import { env } from 'node:process';
 
-import { getPackageBuildConfigs } from '../../tsup.config.base';
+import { defineConfig, Format, Options as TsupConfig } from 'tsup';
 
-export default defineConfig(getPackageBuildConfigs());
+type Platform = 'browser' | 'node' | 'react-native';
+
+type BuildOptions = {
+    format: Format;
+    platform: Platform;
+};
+
+export default defineConfig([
+    getBuildConfig({ format: 'cjs', platform: 'node' }),
+    getBuildConfig({ format: 'esm', platform: 'node' }),
+    getBuildConfig({ format: 'cjs', platform: 'browser' }),
+    getBuildConfig({ format: 'esm', platform: 'browser' }),
+    getBuildConfig({ format: 'esm', platform: 'react-native' }),
+]);
+
+function getBuildConfig(options: BuildOptions): TsupConfig {
+    const { format, platform } = options;
+    return {
+        define: {
+            __BROWSER__: `${platform === 'browser'}`,
+            __ESM__: `${format === 'esm'}`,
+            __NODEJS__: `${platform === 'node'}`,
+            __REACTNATIVE__: `${platform === 'react-native'}`,
+            __TEST__: 'false',
+            __VERSION__: `"${env.npm_package_version}"`,
+        },
+        entry: [`./src/index.ts`],
+        esbuildOptions(options) {
+            options.define = { ...options.define, 'process.env.NODE_ENV': 'process.env.NODE_ENV' };
+        },
+        format,
+        name: platform,
+        outExtension({ format }) {
+            return { js: `.${platform}.${format === 'cjs' ? 'cjs' : 'mjs'}` };
+        },
+        platform: platform === 'node' ? 'node' : 'browser',
+        publicDir: true,
+        pure: ['process'],
+        sourcemap: true,
+    };
+}
